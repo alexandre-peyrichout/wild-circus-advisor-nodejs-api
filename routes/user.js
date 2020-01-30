487840d6fa880f447b3cf7a70f0d545951340ec7
Alexandre Peyrichout <a.peyrichout@gmail.com>
2020-01-30 02:25:09 +0100

    add route auth

--- routes/user.js
+++ routes/user.js
@@ -149,4 +149,26 @@ router.delete("/:id", (req, res) => {
   });
 });
 
+router.post("/verify", verifyToken, (req, res) => {
+  jwt.verify(req.token, "chaussetterouge123", (err, authData) => {
+    if (err) {
+      res.status(403).send("forbidden");
+    } else {
+      res.json(authData);
+    }
+  });
+});
+
+function verifyToken(req, res, next) {
+  const bearerHeader = req.headers.authorization;
+  if (typeof bearerHeader !== undefined) {
+    const bearer = bearerHeader.split(" ");
+    const bearerToken = bearer[1];
+    req.token = bearerToken;
+    next();
+  } else {
+    res.sendStatus(403);
+  }
+}
+
 module.exports = router;
